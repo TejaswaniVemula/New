4bdc55713efc2659088a0f4834b44454d929f6be
TejaswaniVemula <144512754+TejaswaniVemula@users.noreply.github.com>
2025-02-23 11:57:12 +0530

    Create solution_c.c

---
+++ solution_c.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", & t);
+
+    while (t--) {
+        int n, x, sum = 0;
+        scanf("%d %d", & n, & x);
+        int a[n], b[n];
+
+        for (int i = 0; i < n; i++) {
+            scanf("%d", & a[i]);
+        }
+
+        for (int j = 0; j < n; j++) {
+            scanf("%d", & b[j]);
+        }
+        for (int i = 0; i < n; i++) {
+            if (a[i] >= x) sum += b[i];
+        }
+        printf("%d\n", sum);
+    }
+  return 0;
+
+}
